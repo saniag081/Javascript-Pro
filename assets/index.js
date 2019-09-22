38ba89249624fa9aa8bad2771be7614a330147ff
Santiago <sgonzalezhernandez081@gmail.com>
2019-09-22 15:52:43 -0500

    Scope

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,7 @@
+import MediaPlayer from './MediaPlayer.js';
+
 const video = document.querySelector('video')
 const btn = document.querySelector('button')
-
-function MediaPlayer(config){
-    this.media = config.el
-}
-
-MediaPlayer.prototype.play = function(){
-    this.media.play()
-}
-
-MediaPlayer.prototype.pause = function(){
-    this.media.pause()
-}
-MediaPlayer.prototype.togglePlay= function(){
-    if(this.media.paused){
-        this.play()
-    }else{
-        this.pause()
-    }
-}
-
 const player = new MediaPlayer({ el: video})
 
 btn.onclick = ()=>player.togglePlay()
